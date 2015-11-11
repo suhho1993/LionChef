d7d6be0f201bb1502649594d2cbc31f8b0f4a8eb
Suhho_Lee <suhho1993@gmail.com>
2015-11-11 11:37:28 +0900

    CoreLogic has been implemented

--- LionChef/src/logic/CoreLogic.java
+++ LionChef/src/logic/CoreLogic.java
@@ -1,32 +1,31 @@
 package logic;
 
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 import Database.DBConnectorImpl;
+import Database.DishCreater;
 import Exceptions.NoDishException;
 
 /**
- * @author suhho
- * CoreLogic class functionality:
- * -Create a new dish if not already exists in RecipeManager
- * -Create Random Dish if not are parsed by the user
- * -Manage database for the dishes 
+ * @author suhho CoreLogic class functionality: -Create a new dish if not
+ *         already exists in RecipeManager -Create Random Dish if not are parsed
+ *         by the user -Manage database for the dishes
  * 
  */
 public class CoreLogic {
 
 	private Dish currentDish;
-	private DBConnectorImpl DBConnection;
-	
-	
+	private DBConnectorImpl DBConnector;
+	private DishCreater dishCreater;
+
 	/**
-	 * Constructor 
-	 * Creates and opens new database connection
+	 * Constructor Creates and opens new database connection
 	 */
-	public CoreLogic(){
-		DBConnection = new DBConnectorImpl();
+	public CoreLogic() {
+		DBConnector = new DBConnectorImpl();
 	}
-	
+
 	/**
 	 * @return the currentDish
 	 */
@@ -35,31 +34,32 @@ public class CoreLogic {
 	}
 
 	/**
-	 * @param dishList - array of strings
-	 * @exception -NoDishException thrown when dish is not available in the database
-	 *-filters array of String to retrieve one random string
-	 *-searches database for that string
-	 *-sets and returns currentDish returned by the database
+	 * @param dishList
+	 *            - array of strings
+	 * @exception -NoDishException
+	 *                thrown when dish is not available in the database -filters
+	 *                array of String to retrieve one random string -searches
+	 *                database for that string -sets and returns currentDish
+	 *                returned by the database
 	 */
-	public Dish setCurrentDish(ArrayList<String> dishList) throws NoDishException {
-		//TODO
-		return null;
+	public void setCurrentDish(ArrayList<String> dishes) throws NoDishException {
+		String str = RandomFilter.filterStrings(dishes);
+		currentDish = DBConnector.get(str);
 	}
 
 	/**
 	 * @return current database connection
 	 */
 	public DBConnectorImpl getRecipeDatabase() {
-		return DBConnection;
+		return DBConnector;
 	}
 
 	/**
 	 * @param recipeDatabase
-	 * Set current database connection
+	 *            Set current database connection
 	 */
 	public void setRecipeDatabase(DBConnectorImpl recipeDatabase) {
-		this.DBConnection = recipeDatabase;
+		this.DBConnector = recipeDatabase;
 	}
-	
-	
+
 }
